7fe142fd05e2052ce0f3c723ce19cbb0133e9441
JoeyC12 <haoqic2@illinois.edu>
2025-10-07 00:53:51 -0500

    Fix GitHub Actions working directory for my-app

--- my-app/src/pages/Search.tsx
+++ my-app/src/pages/Search.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { TMDBService } from '../services/tmdbService';
 import { Movie, SortOption, SortOrder } from '../types/movie';
